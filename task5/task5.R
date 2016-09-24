52590cdf50ad4a2a5d1ad530f36ea834468af706
Karolina Lenzbergaitė <Karolina Lenzbergait>
2016-09-24 23:35:52 +0300

    task5

--- task5/task5.R
+++ task5/task5.R
@@ -5,35 +5,35 @@ install.packages("prob")
 
 library(prob)
 
-dr<- sample(1:6, size = 3, replace = T)
+n <- 10^5
 
-S <-replicate(10^5, sum(dr))
+S <- replicate(n,sum(sample(1:6, 3, replace = TRUE)))
 
 head(S, 15)
+
 
 ## Teorinis skirstinys
 
 
-St <- rolldie(3, nsides = 6, makespace = T)
+St <- rolldie(3, nsides = 6, makespace = TRUE)
 
 St <- addrv(St, sum = X1 + X2 + X3)
 
-teor<- marginal(distr, vars="sum")
+teor <- marginal(St, vars="sum")
 
 teor
 
-cbind(as.data.frame(table(S)/n), S_teor)
 
-plot(table(S)/n, xlab="Suma", ylab="Tikimybė")
-
-points(S_teor, col=2)
 
 ##Tankis ir santykinis daznis
 
 
-plot(table(allsum)/1000, xlab="Sum", ylab="Prob")
+cbind(as.data.frame(table(S)/n), teor)
 
-lines(theor, col=2)
+plot(table(S)/n, xlab="Suma", ylab="Tikimybė")
+
+points(teor, col=2)
+
 
 ## Vidurkis ir dispersija
 
@@ -42,33 +42,41 @@ average <- sum(teor$probs * teor$sum)
 
 average
 
-dis <- sum((teor$probs * (teor$sum-average)) * (teor$probs * (teor$sum-average)))
 
-dis
+disp <- sum(teor$probs * (teor$sum-average)^2)
 
-plot(cumsum(S)/1:10^5, type="l", xlab="10^5", ylab = "S / 10^5", main="Vidurkio artėjimas į teorinį")
+disp
+
+
+plot(cumsum(S)/1:n, type="l", xlab="n", ylab = "S / n", main="Vidurkio artėjimas į teorinį")
 
 abline(h=average, col=2)
+
 
 a=S*0
 
 for(i in 1:10000){
-
-    a[i]=var(S[1:i])
+  a[i]=var(S[1:i])
 }
 
+plot(a[1:10000], type="l", xlab="n", ylab="varS", main="Dispersijos artėjimas į teorinę")
 
-plot(a[1:10000], type="l", xlab="10^5", ylab="dis", main="Dispersijos artėjimas į teorinę")
+abline(h=disp, col=2)
 
-abline(h=dis, col=2)
 
 ## Sources
 
 
 https://cran.r-project.org/web/packages/IPSUR/vignettes/IPSUR.pdf
+
 http://stackoverflow.com/questions/14820203/simulating-rolling-two-dice
+
 http://ditraglia.com/Econ103Public/Rtutorials/Rtutorial4.html
+
 https://cran.r-project.org/web/packages/dice/dice.pdf
+
 https://cran.r-project.org/web/packages/prob/vignettes/prob.pdf
+
 Emilija Rudytė
+
 Simona Gritytė, Orinta Jautakaitė, Sandra Kozlovskaja, Agnė Zavackaitė
